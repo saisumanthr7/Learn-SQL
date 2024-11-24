3afcf8d3d3b67278b8adf85ee1c35f8cae64b99a
saisumanthr7 <57142654+saisumanthr7@users.noreply.github.com>
2024-11-24 08:51:04 -0600

    Update 5.sql
    I have got an error while working on this problem using your query, but tried to resolve it and optimized the query. Please check and let me know if that works for you. 
    Thank you for your time.

--- solutions/5.sql
+++ solutions/5.sql
@@ -3,3 +3,11 @@ FROM bands
 LEFT JOIN albums ON bands.id = albums.band_id
 GROUP BY albums.band_id
 HAVING COUNT(albums.id) = 0;
+
+The query is causing an error because MySQL in ONLY_FULL_GROUP_BY mode requires all selected columns that are not part of an aggregate function to be included in the GROUP BY clause.
+Below is the optimized query to not use group by:
+
+SELECT bands.name AS 'Band Name'
+FROM bands
+LEFT JOIN albums ON bands.id = albums.band_id
+WHERE albums.band_id IS NULL;
